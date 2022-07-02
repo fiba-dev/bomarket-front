5499b3f2439ea81149047162c5813287c2f22107
Alvax10 <alvaro695547@gmail.com>
2022-07-01 23:37:29 -0300

    fixed responsive problem at itemId

--- components/showItem/styled.tsx
+++ components/showItem/styled.tsx
@@ -2,15 +2,16 @@ import styled from "styled-components";
 
 export const CatalogueLink = styled.div`
 	text-decoration: underline 1px;
-	align-self: flex-start;
 	margin-bottom: 50px;
-	margin-left: 120px;
+	align-self: center;
 	color: goldenrod;
 	margin-top: 50px;
 	cursor: pointer;
 	font-size: 18px;
 
 	@media(min-width: 1080px) {
+		align-self: flex-start;
+		margin-left: 120px;
 		margin-left: 80px;
 		font-size: 20px;
 	}
